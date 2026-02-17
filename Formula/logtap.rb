a6894e641365f1c9a134aaee01b564e77fa76845
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-18 00:51:50 +0800

    chore: update homebrew formula to v1.0.5

--- Formula/logtap.rb
+++ Formula/logtap.rb
@@ -4,28 +4,28 @@
 class Logtap < Formula
   desc "Ephemeral log mirror for load testing"
   homepage "https://github.com/ppiankov/logtap"
-  version "1.0.1"
+  version "1.0.5"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/logtap/releases/download/v1.0.1/logtap_1.0.1_darwin_arm64.tar.gz"
-      sha256 "5c0f14dfa85fc6445133d7b1cccc6cc2591f3df71ea560b8a7afc97f9e6efb9e"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.5/logtap_1.0.5_darwin_arm64.tar.gz"
+      sha256 "78b4d15c0f5d4b000e08d9363510fb07abe6284d8d487c680a2175d4635d93f6"
     end
     on_intel do
-      url "https://github.com/ppiankov/logtap/releases/download/v1.0.1/logtap_1.0.1_darwin_amd64.tar.gz"
-      sha256 "9216451058aa62f05c0378e561cbd3e86b79ce632379b1b34d46b8590aa67ee1"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.5/logtap_1.0.5_darwin_amd64.tar.gz"
+      sha256 "a04796d8b38ebd1c1c9d9655de676e3f4ffae5af88eb7335e5329fe44ac02267"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/logtap/releases/download/v1.0.1/logtap_1.0.1_linux_arm64.tar.gz"
-      sha256 "6b62fd2f9df8255f62250e748f7fbf41801e6d03e0765358873fa26cc7525ab5"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.5/logtap_1.0.5_linux_arm64.tar.gz"
+      sha256 "5760113a5055f4e1a6734914b8c2386c144a0e1131a69ae4db252251a3dfa90e"
     end
     on_intel do
-      url "https://github.com/ppiankov/logtap/releases/download/v1.0.1/logtap_1.0.1_linux_amd64.tar.gz"
-      sha256 "a69198421dd1713726704b223c58eb92afab0628095967abff52861194f65667"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.5/logtap_1.0.5_linux_amd64.tar.gz"
+      sha256 "b620976d7a230f0b05eb3250cd7936f0741b9cc49b2dde06442ea4a267d816cb"
     end
   end
 
