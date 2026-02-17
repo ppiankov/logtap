ee7f428d547679311672e1e836273f423618588d
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-17 13:08:57 +0800

    chore: update homebrew formula to v1.0.0

--- Formula/logtap.rb
+++ Formula/logtap.rb
@@ -4,27 +4,27 @@
 class Logtap < Formula
   desc "Ephemeral log mirror for load testing"
   homepage "https://github.com/ppiankov/logtap"
-  version "VERSION"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ppiankov/logtap/releases/download/vVERSION/logtap_VERSION_darwin_arm64.tar.gz"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.0/logtap_1.0.0_darwin_arm64.tar.gz"
       sha256 "SHA256_DARWIN_ARM64"
     end
     on_intel do
-      url "https://github.com/ppiankov/logtap/releases/download/vVERSION/logtap_VERSION_darwin_amd64.tar.gz"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.0/logtap_1.0.0_darwin_amd64.tar.gz"
       sha256 "SHA256_DARWIN_AMD64"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ppiankov/logtap/releases/download/vVERSION/logtap_VERSION_linux_arm64.tar.gz"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.0/logtap_1.0.0_linux_arm64.tar.gz"
       sha256 "SHA256_LINUX_ARM64"
     end
     on_intel do
-      url "https://github.com/ppiankov/logtap/releases/download/vVERSION/logtap_VERSION_linux_amd64.tar.gz"
+      url "https://github.com/ppiankov/logtap/releases/download/v1.0.0/logtap_1.0.0_linux_amd64.tar.gz"
       sha256 "SHA256_LINUX_AMD64"
     end
   end
